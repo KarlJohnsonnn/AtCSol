1f6215850c36b3b292013374566387c04f408d72
Willi Schimmel <schimmel@tropos.de>
2017-03-08 11:04:43 +0100

    more space

--- MAIN_CHEMIE.f90
+++ MAIN_CHEMIE.f90
@@ -22,21 +22,26 @@ PROGRAM Main_ChemKin
   USE NetCDF_Mod
   IMPLICIT NONE
   !
-  CHARACTER(80) :: Filename0=''        ! *.run file
+  CHARACTER(80)   :: Filename0 = ''        ! *.run file
   !
-  REAL(RealKind) :: Atol(2)
-  INTEGER :: i,j,nSchwefel
-  CHARACTER(80) :: constH=''
-  CHARACTER(80) :: neuTolR=''
-  CHARACTER(80) :: neuTolA=''
-  CHARACTER(80) :: neuROW=''
-  CHARACTER(16) :: tmpchar0='-'
-  REAL(RealKind) :: h 
-  REAL(RealKind), PARAMETER :: HR=3600.0d0
-  REAL(RealKind) :: tmpMW0
-  REAL(RealKind), ALLOCATABLE :: X0(:)
-  INTEGER :: i_error, linc, STAT
+  REAL(RealKind)  :: Atol(2)
+  INTEGER         :: i,j,nSchwefel
+  CHARACTER(80)   :: constH   = ''
+  REAL(RealKind)  :: h 
+  REAL(RealKind), PARAMETER     :: HR=3600.0d0
+  REAL(RealKind), ALLOCATABLE   :: X0(:)
+  INTEGER         :: i_error, linc, STAT
   !
+  ! use other ROS methode or tolerance
+  CHARACTER(80)   :: neuTolR  = ''
+  CHARACTER(80)   :: neuTolA  = ''
+  CHARACTER(80)   :: neuROW   = ''
+
+  ! temp variables for molecular weights
+  CHARACTER(16)   :: tmpchar0 = '-'
+  REAL(RealKind)  :: tmpMW0
+  INTEGER         :: tmpPos    
+
   !
   !================================================================
   !===                     MAIN Programm
@@ -85,14 +90,14 @@ PROGRAM Main_ChemKin
   LWCBounds(5)=LWCBounds(4) + 0.25d0*HR
   LWCBounds(6)=LWCBounds(5) + 1.d0*HR
   !----------------------------------------------------------------
-!  --- read the .sys data, save coefs in sparse matrix
+  !  --- read the .sys data, save coefs in sparse matrix
+  Time_Read=MPI_WTIME()
   OPEN(UNIT=89,FILE=ADJUSTL(TRIM(ChemFile))//'.chem',STATUS='UNKNOWN')
   IF (MPI_ID==0) WRITE(*,'(A38)',ADVANCE='NO') '  Read sys-file ................ done'
   IF (ChemFile(1:2)=='ck') THEN
     ! *)
     IF (MPI_ID==0) WRITE(*,*) ' ---->  Solve Gas Energy Equation '
     combustion=.TRUE.
-    Time_Read=MPI_WTIME()
     CALL Read_Elements    ( ChemFile    , 969 )
     CALL Read_Species     ( ChemFile    , 969 )
     CALL Read_Reaction    ( ChemFile    , 969 )
@@ -103,158 +108,151 @@ PROGRAM Main_ChemKin
     CALL PrintHeadReactions ( 89 )
     CALL PrintReactions     ( ReactionSystem , 89 , .TRUE. )       ! .TRUE. in 3rd agument for chemkin input file
     CALL PrintFinalReactions( 89 )
-    !
+   
     !--- Read molecular mass
-    OPEN(UNIT=998,FILE=TRIM(ChemFile)//'.mw',STATUS='UNKNOWN')
-    ALLOCATE(MW(nspc),rMW(nspc))
-    MW=ZERO
-    !DO i=1,nspc
-    DO
-      READ(998,*,IOSTAT=STAT) tmpChar0, tmpMW0
-      IF ( STAT > 0 ) EXIT
-      IF ( PositionSpeciesAll(tmpChar0) > 0 ) THEN
-        MW(PositionSpeciesAll(tmpChar0))=REAL(tmpMW0,RealKind)
-      END IF
-    END DO
-    rMW(:)=ONE/MW(:)
-    !DO i=1,nspc
-    !  print*, 'debug:: main    ',i, MW(i),rMW(i)
-    !END DO
+    OPEN ( UNIT=998 , FILE=TRIM(ChemFile)//'.mw' , STATUS='UNKNOWN' )
+    ALLOCATE( MW(nspc) , rMW(nspc) )  
+    MW(:)   = ZERO
+    rMW(:)  = ZERO
     
-    CLOSE(linc)
+    DO
+      READ( 998 , * , IOSTAT=STAT ) tmpChar0 , tmpMW0
+      IF ( STAT > 0 )   EXIT
+      tmpPos  = PositionSpeciesAll( tmpChar0 )
+      IF ( tmpPos > 0 ) MW(tmpPos) = REAL( tmpMW0 , RealKind )
+    END DO
+    rMW(:) = ONE / MW(:)
+    CLOSE ( 998 )
+    !
     !
     !ALLOCATE(InitValAct(ntGas),y_e(ntGas))
-    ALLOCATE(X0(ntGas),InitValAct(ntGas),y_e(ntGas))
-    X0=ZERO           ! mole fraction 
-    InitValAct=ZERO   ! mol/m3 
-    ALLOCATE(InitValKat(ntKat))
-    !CALL Read_GASini(InitFile,InitValAct,InitValKat)
-    CALL Read_GASini(InitFile,X0,InitValKat)
-    CALL Read_EMISS(InitFile,y_e)
-    CALL GetSpeciesNames(ChemFile,y_name)
+    ALLOCATE( X0(ntGas) , InitValAct(ntGas) , y_e(ntGas) )
+    ALLOCATE( InitValKat(ntKat) )
+    X0          = ZERO           ! mole fraction 
+    InitValAct  = ZERO   ! mol/m3 
+
+    CALL Read_GASini    ( InitFile , X0 , InitValKat )
+    CALL Read_EMISS     ( InitFile , y_e )
+    CALL GetSpeciesNames( ChemFile , y_name )
     !CALL MassFr_to_MoleFr(InitValAct,X0)
     !CALL MoleFr_to_MassFr(InitValAct,X0)
     !
-    CALL MoleFr_to_MoleVol(InitValAct,X0,2.0d0)
+    CALL MoleFr_to_MoleVol( InitValAct , X0 , 2.0d0 )
     !
     !--- richtigen index holen, da TB unsortiert eingelesen
-    DO i=1,neq
+    DO i = 1 , neq
       IF (ALLOCATED(ReactionSystem(i)%TB)) THEN
         DO j=1,SIZE(ReactionSystem(i)%TB)
-          ReactionSystem(i)%TB(j)=PositionSpeciesAll(ReactionSystem(i)%TBspc(j)) 
+          ReactionSystem(i)%TB(j) = PositionSpeciesAll(ReactionSystem(i)%TBspc(j)) 
         END DO
       END IF
     END DO
     ! debug speedchem reihenfolge der species für besseres debuggen
     scPermutation=(/26,28,25,18,21,17,20,29,22,24,19,27,15,13,23,12,14,16,1,2,3,4,5,6,7,9,10,11,8/)
   ELSE
-    IF (MPI_ID==0) WRITE(*,*) ' ---->  Fix Temperature'
-    Time_Read=MPI_WTIME()
-    CALL ReadSystem(ChemFile)
-    !CALL InputDatFile(DataFile)
+    IF ( MPI_ID==0 ) WRITE(*,*) ' ---->  Fix Temperature'
+    CALL ReadSystem( ChemFile )
     !   
     !----------------------------------------------------------------
     ! ---  build the coeficient matrices and write .chem
-    CALL PrintHeadSpecies(ChemFile,89)
-    CALL PrintSpecies(ListGas2,89)
-    CALL PrintSpecies(ListAqua2,89)
-    CALL PrintSpecies(ListSolid2,89)
-    CALL PrintSpecies(ListPartic2,89)
-    CALL PrintSpecies(ListNonReac2,89)
-    CALL PrintHeadReactions(89)
+    CALL PrintHeadSpecies ( ChemFile      , 89 )
+    CALL PrintSpecies     ( ListGas2      , 89 )
+    CALL PrintSpecies     ( ListAqua2     , 89 )
+    CALL PrintSpecies     ( ListSolid2    , 89 ) 
+    CALL PrintSpecies     ( ListPartic2   , 89 )
+    CALL PrintSpecies     ( ListNonReac2  , 89 )
+    CALL PrintHeadReactions( 89 )
     ! 
     !----------------------------------------------------------------
     ! --- Build the reaction system
-    CALL AllListsToArray( ReactionSystem           &
-    &                    ,ListRGas                 &
-    &                    ,ListRHenry               &
-    &                    ,ListRAqua                &
-    &                    ,ListRDiss                )
+    CALL AllListsToArray  ( ReactionSystem     &
+    &                     , ListRGas           &
+    &                     , ListRHenry         &
+    &                     , ListRAqua          &
+    &                     , ListRDiss          )
     !
-    !CALL CheckConstants(ReactionSystem)
     !----------------------------------------------------------------
     ! --- print reactions and build A, B and (B-A) structure
-    CALL PrintReactions(ReactionSystem,89)
-    CALL PrintFinalReactions(89)
+    CALL PrintReactions   ( ReactionSystem , 89 )
+    CALL PrintFinalReactions( 89 )
     !
     !----------------------------------------------------------------
     ! --- Input of initial data and thermodynamic properties
-    CALL InputChemicalData(InitFile,DataFile,MetFile)
+    CALL InputChemicalData( InitFile , DataFile , MetFile )
     ! 
   END IF
   CLOSE(89)
-  ! Stop Input timer
-  Time_Read=MPI_WTIME()-Time_Read
+
+  !----------------------------------------------------------------
+  ! --- Read species for diagnose (print species concentrations to NetCDF file)
+  CALL Read_Diag( OutNetcdfSpc , OutNetcdfPhase , InitFile )
+
+  Time_Read = MPI_WTIME() - Time_Read  ! Stop Input timer
+
   IF (MPI_ID==0) WRITE(*,*) '  Read ini-file ................ done'
   IF (MPI_ID==0) WRITE(*,*) '  Print chem-file .............. done'
-  !
+
   !----------------------------------------------------------------
   ! --- Split the rate array in mpi_np parts 
-  CALL BuildPartitions(MyParties,neq,MPI_np)
-  !
-  !
-  !----------------------------------------------------------------
-  ! ---  print some species (set output spc in InitFile under DIAG)
-  CALL Read_Diag(OutNetcdfSpc,OutNetcdfPhase,InitFile)
-  !
-  !
+  CALL BuildPartitions( MyParties , neq , MPI_np )
+
+
   !-----------------------------------------------------------------------
   ! --- Start timer and set absolut tolerance for species
-  Timer_Start=MPI_WTIME()
-  Atol=(/AtolGas,AtolAqua/)
-  ! 
-  nsr=nspc+neq
+  Timer_Start = MPI_WTIME()
+  Atol        = (/ AtolGas , AtolAqua /)
+ 
+  nsr = nspc + neq
+  
+  !-----------------------------------------------------------------------
+  ! --- Dimension initialisation for the unknowns and matrices
   IF ( combustion ) THEN
-    nDIM=nspc+1
-    nDIMcl=nspc+1
-    nDIMex=nspc+neq+1
+    nDIM    = nspc + 1
+    nDIMcl  = nspc + 1
+    nDIMex  = nspc + neq + 1
   ELSE
-    nDIM=nspc
-    nDIMcl=nspc
-    nDIMex=nspc+neq
+    nDIM    = nspc
+    nDIMcl  = nspc
+    nDIMex  = nspc + neq
   END IF
-    !
-  ! 
-  CALL getarg(2,neuTolR)             
-  CALL getarg(3,neuTolA)             
-  IF (neuTolR/='')   THEN
-    READ(neuTolR,*) RtolROW
+
+  !-----------------------------------------------------------------------
+  ! --- If more than one argument is passed set new tolerance and ROS methode
+  CALL getarg ( 2 , neuTolR )             
+  IF ( neuTolR /= '' )  READ( neuTolR , * ) RtolROW
+
+  CALL getarg ( 3 , neuTolA )             
+  IF ( neuTolA /= '' )  READ( neuTolA , * ) AtolGas
+
+  CALL getarg ( 4 , neuTolA )             
+  IF ( neuTolA /= '' )  READ( neuTolA , * ) AtolAqua
+ 
+  CALL getarg ( 5 , neuROW )
+  IF ( neuROW /= '' )   THEN  
+    READ( neuRow , * ) RosenbrockMethod
+    RosenbrockMethod = ADJUSTL('METHODS/'//RosenbrockMethod)
   END IF
-  IF (neuTolA/='')   THEN
-    READ(neuTolA,*) AtolGas
-    AtolAqua=AtolGas
-  END IF
-  !
-  CALL getarg(4,neuROW)
-  IF (neuROW/='')   THEN
-    READ(neuRow,*) RosenbrockMethod
-    RosenbrockMethod=ADJUSTL('METHODS/'//RosenbrockMethod)
-  END IF
-  !
-  ! hole alle schwefelstoffe
-  nSchwefel=0
-  DO i=1,nspc
-    IF (INDEX(y_name(i),'S')>0) THEN
-      nSchwefel=nSchwefel+1
-    END IF
+  
+  !-----------------------------------------------------------------------
+  !--- get all sulphuric species
+  nSchwefel = 0
+  DO i = 1 , nspc
+    IF ( INDEX(y_name(i),'S') > 0 ) nSchwefel = nSchwefel + 1
   END DO
+
   ALLOCATE(iDiag_Schwefel(nSchwefel))
-  nSchwefel=0
+  nSchwefel = 0
   DO i=1,nspc
-    IF (INDEX(y_name(i),'S')>0) THEN
-      nSchwefel=nSchwefel+1
-      iDiag_Schwefel(nSchwefel)=PositionSpeciesAll(y_name(i))
+    IF ( INDEX(y_name(i),'S') > 0 ) THEN
+      nSchwefel = nSchwefel + 1
+      iDiag_Schwefel(nSchwefel) = PositionSpeciesAll( y_name(i) ) 
     END IF
   END DO
-  !
-    !print*, 'debug :: nach schwefel'
-    !do i=1,nspc
-      !print*, y_name(i),InitValAct(i)
-      !WRITE(111,*) y_name(i)
-    !end do
-    !stop
-  !
+  
+  !-----------------------------------------------------------------------
+  !--- print input parameter, method, tols, etc.
   CALL Print_Run_Param()
+
   !-----------------------------------------------------------------------
   ! --- choose between implicit Euler or some Rosenbrock-Wanner methode
   IF (ImpEuler==1) THEN
@@ -268,9 +266,7 @@ PROGRAM Main_ChemKin
     !!
     !READ(ConstH,*) h
     !CALL IntegrateEuler(y_iconc(1:nspc),Tspan,h)
-    IF (MPI_ID==0) THEN
-      WRITE(*,*) ' Currently not available....'
-    END IF
+    IF (MPI_ID==0)  WRITE(*,*) ' Implicit Euler currently not available....'
     CALL Dropout()
   ELSE
     CALL Integrate (  InitValAct(1:nspc)   &     ! initial concentrations activ species
@@ -283,13 +279,15 @@ PROGRAM Main_ChemKin
   !
   !---------------------------------------------------------------
   ! --- stop timer and print output statistics
-  Timer_Finish=MPI_WTIME()
-  TimeIntegrationE=(TimeIntegrationE-TimeIntegrationA)
-  Timer_Finish=(Timer_Finish-Timer_Start)+Time_Read
+  Timer_Finish      = MPI_WTIME()
+  TimeIntegrationE  = TimeIntegrationE - TimeIntegrationA
+  Timer_Finish      = Timer_Finish - Timer_Start + Time_Read
   !
   ! Print statistics
-  CALL Output_Statistics( Time_Read, TimeSymbolic, TimeFac, TimeSolve, TimeRates,         &
-  &                       TimeJac, TimeIntegrationE,Timer_Finish,TimeRateSend, TimeNetCDF )
+  CALL Output_Statistics( Time_Read       , TimeSymbolic  , TimeFac       &
+  &                     , TimeSolve       , TimeRates     , TimeJac       &
+  &                     , TimeIntegrationE, Timer_Finish  , TimeRateSend  &
+  &                     , TimeNetCDF                                      )
   WRITE(*,*) ''
   !---------------------------------------------------------------
   ! --- Close MPI 
